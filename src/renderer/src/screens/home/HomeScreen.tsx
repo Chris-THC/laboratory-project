94143de4c993e47b094822f231e51efd92f4064d
Chris <104177004+Chris-THC@users.noreply.github.com>
2024-03-01 19:47:25 -0600

    Navbar was created but doesn't work yet (#1)
    Navbar was created but doesn't work yet

--- src/renderer/src/screens/home/HomeScreen.tsx
+++ src/renderer/src/screens/home/HomeScreen.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { UserAdmin } from '../clients/UserAdmin'
 import { UserEmployee } from '../clients/UserEmployee'
+import { NavBar } from '@renderer/components/NavComponent/NavBar'
 export const HomeScreen: React.FC = () => {
   return (
     <div>
+      <NavBar />
       <Routes>
         <Route path="/" element={<UserAdmin />} />
         <Route path="/employee" element={<UserEmployee />} />
