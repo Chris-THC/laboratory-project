70b2ebf1b83dcdef69ee6a3aa1946daa161cdb83
Chris CR <chris_49git@outlook.com>
2024-03-03 12:10:47 -0600

    Users CRUD is working

--- src/renderer/src/screens/users/DeleteUser.tsx
+++ src/renderer/src/screens/users/DeleteUser.tsx
@@ -23,8 +23,8 @@ export const DelateUserModal: React.FC = () => {
   const onDeleteUser = (): void => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const idUser: any = userObjectInfo?.idUser
-    console.log(idUser);
-    
+    console.log(idUser)
+
     deleteUser.mutate(idUser)
     navigateTo('/users')
   }
@@ -41,10 +41,7 @@ export const DelateUserModal: React.FC = () => {
           <AlertDialogTitle className="flex flex-row justify-center align-middle">
             ¿Está seguro de eliminar a este usuario?
           </AlertDialogTitle>
-          <AlertDialogDescription className="text-center">
-            El usuario con el registrado con el nombre Juan Pérez Hernandez se eliminará de la
-            aplicación
-          </AlertDialogDescription>
+          <AlertDialogDescription className="text-center">{`El usuario con el registrado con el nombre ${userObjectInfo?.name} se eliminará de la aplicación`}</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <div className="flex justify-center align-middle w-full">
