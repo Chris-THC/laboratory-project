a65045985546b2c212e0761500c214ca6f0a6132
Israel <NARUTO-ONE@LIVE.COM.MX>
2024-03-03 22:22:02 -0600

    created new client interface

---
+++ src/renderer/src/interfaces/clients/clients.d.ts
@@ -0,0 +1,10 @@
+export interface ClientsInterface {
+    idUser?: number | undefined | null
+    name: string
+    age: number
+    phoneNumber: string
+    address: string
+    password: string
+    role: number
+  }
+  
