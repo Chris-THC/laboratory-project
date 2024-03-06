7cc788a316cc499b51c020ba57a806bdca588af2
Chris CR <chris_49git@outlook.com>
2024-03-06 15:21:55 -0600

    CRUD users is work

--- src/renderer/src/api/ConnectionAPI.ts
+++ src/renderer/src/api/ConnectionAPI.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
 const urlAPI: string = 'http://localhost:8081/lab'
 
-const jwtToken: string =
-  'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKdWFuIExvcGV6IEhlcm5hbmRleiIsImlkVXNlciI6MiwidXNlck5hbWUiOiJKdWFuIExvcGV6IEhlcm5hbmRleiIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTcwOTc0OTg1NiwiZXhwIjoxNzA5NzUxMjk2fQ.SOK_F3bUtlBPLvkt-fTwoJdLEBo-1Dp3RHDXhL1lG7g'
+const jwtToken: string = 'add_token'
 
 const api = axios.create({
   baseURL: urlAPI,
