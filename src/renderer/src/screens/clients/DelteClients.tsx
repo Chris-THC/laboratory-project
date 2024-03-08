9d0103503f443f1873e20fc966a21dca5e844111
Israel <NARUTO-ONE@LIVE.COM.MX>
2024-03-08 00:11:04 -0600

    update delete clients

--- src/renderer/src/screens/clients/DelteClients.tsx
+++ src/renderer/src/screens/clients/DelteClients.tsx
@@ -10,24 +10,26 @@ import {
   AlertDialogTrigger
 } from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
-import { useUserIdSelected } from '@renderer/context/userContext/UserContext'
+import { useClientIdSelected } from '@renderer/context/clientContext/clientContext'
+import { useDelateClient } from '@renderer/hooks/res/clientRes/UseClientAPI'
 import { UserX } from 'lucide-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const DelateClientsModal: React.FC = () => {
-  //   const navigateTo = useNavigate()
-  const { userObjectInfo } = useUserIdSelected()
+  const navigateTo = useNavigate()
+  const { clientObjectInfo, setClientObjectInfo, setIsClientCreate } = useClientIdSelected()
 
-  //   const deleteUser = useDelateUser()
+  const deleteClient = useDelateClient()
   const onDeleteUser = (): void => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    // const idUser: any = userObjectInfo?.idUser
-    // console.log(idUser)
+  const idClient: any = clientObjectInfo?.idCustomer
+    console.log(idClient)
 
-    // deleteUser.mutate(idUser)
-    // setUserObjectInfo(null)
-    // setIsCreate(true)
-    // navigateTo('/users')
+    deleteClient.mutate(idClient)
+    setClientObjectInfo(null)
+    setIsClientCreate(true)
+    navigateTo('/customer')
     console.log('Eliminar')
   }
 
@@ -43,7 +45,7 @@ export const DelateClientsModal: React.FC = () => {
           <AlertDialogTitle className="flex flex-row justify-center align-middle">
             ¿Está seguro de eliminar a este cliente?
           </AlertDialogTitle>
-          <AlertDialogDescription className="text-center">{`El cliente registrado con el nombre ${userObjectInfo?.name} se eliminará de la aplicación`}</AlertDialogDescription>
+          <AlertDialogDescription className="text-center">{`El cliente registrado con el nombre ${clientObjectInfo?.name} se eliminará de la aplicación`}</AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <div className="flex justify-center align-middle w-full">
