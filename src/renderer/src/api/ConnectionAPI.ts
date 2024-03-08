62028594a0e28b5ecaebe0b546268f158b6f39e4
Chris CR <chris_49git@outlook.com>
2024-03-07 18:15:43 -0600

    save token

--- src/renderer/src/api/ConnectionAPI.ts
+++ src/renderer/src/api/ConnectionAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const urlAPI: string = 'http://localhost:8081/lab'
 
-const jwtToken: string = 'add_token'
+const jwtToken: string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MDk3NzYzNDgsImV4cCI6MTcwOTgxMjM0OH0.aOig2l1BEkobS_e_5tSKAh1uVqK6C0d2KUiy4lBLvjA'
 
 const api = axios.create({
   baseURL: urlAPI,
