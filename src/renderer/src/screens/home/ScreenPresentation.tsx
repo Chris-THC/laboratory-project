326134adc3097af6cc2cbc9a172bcc623bb3198a
Chris CR <chris_49git@outlook.com>
2024-03-12 09:16:30 -0600

    update

--- src/renderer/src/screens/home/ScreenPresentation.tsx
+++ src/renderer/src/screens/home/ScreenPresentation.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import image from 'image.jpg'
+import React from 'react'
+import imageMain from '../../assets/img/image.jpg'
 
-const ScreenPresentation: React.FC = () => {
+export const ScreenPresentation: React.FC = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-cover bg-center bg-no-repeat">
-        <img src={image} alt="" />
-        <div className="bg-white bg-opacity-75 p-8 rounded-lg shadow-lg">
-        <h1 className="text-5xl font-bold text-gray-800">Bienvenido a la aplicación Laboratorio Elisa</h1>
-        <p className="mt-4 text-gray-600 text-center"> Aquí podras realizar todas las operaciones sobre tus clientes y empleados.</p>
+    <div className="flex h-screen items-center justify-center">
+      <img src={imageMain} alt="img" className="h-[400px] w-[500px]" />
+      <div className="bg-white bg-opacity-75 rounded-lg shadow-lg">
+        <h1 className="text-5xl font-bold text-gray-800">
+          Bienvenido a la aplicación Laboratorio Elisa
+        </h1>
+        <p className="mt-4 text-gray-600 text-center">
+          Aquí podras realizar todas las operaciones sobre tus clientes y empleados.
+        </p>
       </div>
     </div>
-  );
-};
-
-export default ScreenPresentation;
+  )
+}
