1e1db6ea582e5338a2bb72354d81bee42c1fea52
Chris CR <chris_49git@outlook.com>
2024-05-07 20:23:54 -0600

    Update token

--- src/renderer/src/api/ConnectionAPI.ts
+++ src/renderer/src/api/ConnectionAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const urlAPI: string = 'http://localhost:8081/lab'
 
-const jwtToken: string = ''
+const jwtToken: string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MTUxMzQ5OTksImV4cCI6MTcxNTE3MDk5OX0.XF2j-64rpGw2jbNcayr3wgMIt0o7FsNF6wmFoyod_ic'
 
 const api = axios.create({
   baseURL: urlAPI,
