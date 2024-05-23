98a0228b9e40dcb11c883a6c832d31d9f66c74eb
Chris CR <chris_49git@outlook.com>
2024-05-23 15:33:22 -0600

    Started the edit pdf

--- src/renderer/src/api/ConnectionAPI.ts
+++ src/renderer/src/api/ConnectionAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const urlAPI: string = 'http://localhost:8081/lab'
 
-const jwtToken: string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MTY0MzM5ODIsImV4cCI6MTcxNjQ2OTk4Mn0.-vnAGR-oNu5omEWcej_kUea1BGgIDgk0-JknEJvPMks'
+const jwtToken: string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MTY0OTg5MjYsImV4cCI6MTcxNjUzNDkyNn0.ETNUEdIerN0fw-x-gCqCjc59Qi7ptf6YdcLN29n56gQ'
 
 const api = axios.create({
   baseURL: urlAPI,
