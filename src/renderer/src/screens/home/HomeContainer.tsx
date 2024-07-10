c9b9b7339f7305b243387b554e2385717ce665fc
Chris CR <chris_49git@outlook.com>
2024-07-10 13:09:40 -0600

    add search by customer name and doctor name

--- src/renderer/src/screens/home/HomeContainer.tsx
+++ src/renderer/src/screens/home/HomeContainer.tsx
@@ -5,7 +5,7 @@ export const HomeContainer: React.FC = () => {
   return (
     <div>
       <div className="flex justify-center m-0 p-0">
-        <h2 className='font-inter text-base font-semibold'>Pagina de incio</h2>
+        <h2 className="font-inter text-base font-semibold">Página principal</h2>
       </div>
       <div className="flex flex-1 justify-center items-center content-center flex-col">
         <AddClients />
