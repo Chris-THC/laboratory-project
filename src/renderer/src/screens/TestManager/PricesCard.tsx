493aef2440db112a3a57cf45728bbc04bda6cb8f
Chris CR <chris_49git@outlook.com>
2024-07-26 20:36:16 -0600

    I added the reducer to get total prices

--- src/renderer/src/screens/TestManager/PricesCard.tsx
+++ src/renderer/src/screens/TestManager/PricesCard.tsx
@@ -5,6 +5,10 @@ import { useTestArrayList } from '@renderer/context/testByUser/testArrayByUser'
 export const PricesCard: React.FC = () => {
   const { testArrayList } = useTestArrayList()
 
+  const totalTest = testArrayList!.reduce((acc, testInfo) => {
+    return acc + testInfo.testDTO.testPrice
+  }, 0)
+
   return (
     <div className="container mx-auto px-4 md:px-6">
       {!testArrayList || testArrayList.length === 0 ? (
@@ -12,28 +16,30 @@ export const PricesCard: React.FC = () => {
       ) : (
         <div>
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {testArrayList!.map((testInfo, index) => (
-              <div
-                key={index}
-                className="rounded-lg bg-card p-4 shadow-md transition-all duration-300 hover:shadow-lg"
-              >
-                <div className="mt-4">
-                  <h3 className="text-base font-medium">{testInfo.testDTO.testName}</h3>
+            {testArrayList!.map((testInfo, index) => {
+              return (
+                <div
+                  key={index}
+                  className="rounded-lg bg-card p-4 shadow-md transition-all duration-300 hover:shadow-lg"
+                >
+                  <div className="mt-4">
+                    <h3 className="text-base font-medium">{testInfo.testDTO.testName}</h3>
 
-                  <div className="mt-1 flex items-center justify-between">
-                    <div className="text-muted-foreground flex flex-row items-center h-[30px]">
-                      <p className="text-base font-inter mr-2">{'Precio:'}</p>
-                      <Input
-                        type="text"
-                        defaultValue={testInfo.testDTO.testPrice}
-                        // onChange={(e) => handlePriceChange(product.id, parseFloat(e.target.value))}
-                        className="w-24 border-b border-muted-foreground bg-transparent text-muted-foreground focus:outline-none"
-                      />
+                    <div className="mt-1 flex items-center justify-between">
+                      <div className="text-muted-foreground flex flex-row items-center h-[30px]">
+                        <p className="text-base font-inter mr-2">{'Precio sugerido $'}</p>
+                        <Input
+                          type="text"
+                          defaultValue={testInfo.testDTO.testPrice}
+                          // onChange={(e) => handlePriceChange(product.id, parseFloat(e.target.value))}
+                          className="w-24 border-b border-muted-foreground bg-transparent text-muted-foreground focus:outline-none"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+            })}
           </div>
           <div className="mt-8 flex justify-end">
             <div>
@@ -42,7 +48,7 @@ export const PricesCard: React.FC = () => {
                 variant={'outline'}
                 className="rounded-lg  px-6 py-4 text-xl font-medium text-[#111] shadow-lg"
               >
-                Total: ${700}
+                Total: ${totalTest}
               </Button>
             </div>
           </div>
