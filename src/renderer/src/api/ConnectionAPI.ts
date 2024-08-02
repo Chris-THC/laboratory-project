e3b2ace4a71a23c397027c82a1f9602f326e8af3
Chris CR <chris_49git@outlook.com>
2024-08-01 19:35:30 -0600

    Agregar resumen de pagos

--- src/renderer/src/api/ConnectionAPI.ts
+++ src/renderer/src/api/ConnectionAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const urlAPI: string = 'http://localhost:8081/lab'
 
-const jwtToken: string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MjIzNzE4NzMsImV4cCI6MTcyMjQwNzg3M30.6Mo1abGqBE9sA3DlUgF5dKcage0iDwDjMsDO0Uhr6Ik'
+const jwtToken: string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MjI1NjEwNzksImV4cCI6MTcyMjU5NzA3OX0.Tv3Z13P3YFdzi0eGSFKJSkyZG9jPmVyHhffHvtyd1ws'
 
 const api = axios.create({
   baseURL: urlAPI,
