2b543341a0376fb5d2c2589f8547917029119510
Israel Montiel <NARUTO-ONE@LIVE.COM.MX>
2024-08-12 19:30:20 -0600

    login

--- src/renderer/src/api/ConnectionAPI.ts
+++ src/renderer/src/api/ConnectionAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const urlAPI: string = 'http://localhost:8081/lab'
 
-const jwtToken: string = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MjMxNzk1NjcsImV4cCI6MTcyMzIxNTU2N30.-QgrTM1q8dB-F9tPvU5KRJvvdR5RmrqzzdQhbIBsAqg"
+const jwtToken: string = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJEYXZpZCBHdXN0YXZvIExhcmEgVmVsYXpxdWV6IiwiaWRVc2VyIjoxLCJ1c2VyTmFtZSI6IkRhdmlkIEd1c3Rhdm8gTGFyYSBWZWxhenF1ZXoiLCJyb2xlIjoiQWRtaW4iLCJpYXQiOjE3MjM1MTIyNjgsImV4cCI6MTcyMzU0ODI2OH0.YUGQ1YklF5kb-Ld-GpKVrPI9RuV8kMtqaxAoI_ib8UQ"
 
 const api = axios.create({
   baseURL: urlAPI,
